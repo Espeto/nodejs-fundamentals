e283abdd9c9f1c6dc97fbd3b860b623f4c476097
Espeto <gkreisler5@hotmail.com>
2020-07-02 00:56:01 -0300

    fix: get balance logic

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -9,15 +9,8 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
-  private balance: Balance;
-
   constructor() {
     this.transactions = [];
-    this.balance = {
-      income: 0,
-      outcome: 0,
-      total: 0,
-    };
   }
 
   public all(): Transaction[] {
@@ -25,7 +18,31 @@ class TransactionsRepository {
   }
 
   public getBalance(): Balance {
-    return this.balance;
+    const balance = this.transactions.reduce(
+      (accumulator: Balance, transaction: Transaction) => {
+        switch (transaction.type) {
+          case 'income':
+            accumulator.income += transaction.value;
+            break;
+          case 'outcome':
+            accumulator.outcome += transaction.value;
+            break;
+          default:
+            break;
+        }
+
+        return accumulator;
+      },
+      {
+        income: 0,
+        outcome: 0,
+        total: 0,
+      },
+    );
+
+    balance.total = balance.income - balance.outcome;
+
+    return balance;
   }
 
   public create(): Transaction {
